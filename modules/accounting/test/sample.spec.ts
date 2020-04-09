3a0e28630ed4e795ddc05da3e50c48d933fb20fe
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2020-04-09 18:08:45 -0300

    First commit

---
+++ modules/accounting/test/sample.spec.ts
@@ -0,0 +1,8 @@
+describe('something', () => {
+    describe('The way it smells', () => {
+        it('is pungent', () => {
+            const smell = 'pungent';
+            expect(smell).toBe(smell);
+        });
+    });
+});
