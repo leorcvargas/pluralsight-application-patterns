159f5ce26ea5f2d937e6c6c1c87837a53866c094
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2020-04-11 16:42:12 -0300

    Replace tslint with eslint

--- modules/membership/models/user.ts
+++ modules/membership/models/user.ts
@@ -1,44 +1,44 @@
 import { generateRandomUUID } from '../lib/utility';
 
 interface UserArgs {
-    email: string;
-    createdAt?: Date;
-    status?: string;
-    signInCount?: number;
-    lastLoginAt?: Date;
-    currentLoginAt?: Date;
-    currentSessionToken?: string;
-    reminderSentAt?: Date;
-    reminderToken?: string;
-    authenticationToken?: string;
+  email: string;
+  createdAt?: Date;
+  status?: string;
+  signInCount?: number;
+  lastLoginAt?: Date;
+  currentLoginAt?: Date;
+  currentSessionToken?: string;
+  reminderSentAt?: Date;
+  reminderToken?: string;
+  authenticationToken?: string;
 }
 
 export default class User {
-    public email: string;
-    public createdAt: Date;
-    public status: string;
-    public signInCount: number;
-    public lastLoginAt: Date;
-    public currentLoginAt: Date;
-    public authenticationToken: string;
+  public email: string;
+  public createdAt: Date;
+  public status: string;
+  public signInCount: number;
+  public lastLoginAt: Date;
+  public currentLoginAt: Date;
+  public authenticationToken: string;
 
-    constructor(args: UserArgs) {
-        const {
-            email,
-            createdAt,
-            status,
-            signInCount,
-            lastLoginAt,
-            currentLoginAt,
-            authenticationToken,
-        } = args;
+  constructor(args: UserArgs) {
+    const {
+      email,
+      createdAt,
+      status,
+      signInCount,
+      lastLoginAt,
+      currentLoginAt,
+      authenticationToken,
+    } = args;
 
-        this.email = email;
-        this.createdAt = createdAt || new Date();
-        this.status = status || 'pending';
-        this.signInCount = signInCount || 0;
-        this.lastLoginAt = lastLoginAt || new Date();
-        this.currentLoginAt = currentLoginAt || new Date();
-        this.authenticationToken = authenticationToken || generateRandomUUID();
-    }
+    this.email = email;
+    this.createdAt = createdAt || new Date();
+    this.status = status || 'pending';
+    this.signInCount = signInCount || 0;
+    this.lastLoginAt = lastLoginAt || new Date();
+    this.currentLoginAt = currentLoginAt || new Date();
+    this.authenticationToken = authenticationToken || generateRandomUUID();
+  }
 }
